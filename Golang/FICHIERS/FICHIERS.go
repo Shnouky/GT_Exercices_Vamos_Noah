77b10ffe6ee15a111873bf400b1aa4572e99dc7c
Shnouky <113009584+Shnouky@users.noreply.github.com>
2023-02-27 18:14:38 +0100

    ajout des excercie footer et menu

---
+++ Golang/FICHIERS/FICHIERS.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io/ioutil"
+    "os"
+)
+
+func main() {
+    for {
+        fmt.Println("Que voulez-vous faire ?")
+        fmt.Println("1. Récupérer le contenu du fichier")
+        fmt.Println("2. Ajouter du texte dans le fichier")
+        fmt.Println("3. Supprimer le contenu du fichier")
+        fmt.Println("4. Remplacer le contenu du fichier")
+        fmt.Println("5. Quitter")
+
+        var choice int
+        fmt.Scan(&choice)
+
+        switch choice {
+        case 1:
+            content, err := ioutil.ReadFile("file.txt")
+            if err != nil {
+                fmt.Println("Erreur lors de la lecture du fichier :", err)
+            } else {
+                fmt.Println(string(content))
+            }
+        case 2:
+            fmt.Println("Veuillez entrer le texte à ajouter :")
+            scanner := bufio.NewScanner(os.Stdin)
+            scanner.Scan()
+            text := scanner.Text()
+
+            file, err := os.OpenFile("file.txt", os.O_APPEND|os.O_WRONLY, 0644)
+            if err != nil {
+                fmt.Println("Erreur lors de l'ouverture du fichier :", err)
+            } else {
+                _, err = file.WriteString(text)
+                if err != nil {
+                    fmt.Println("Erreur lors de l'écriture dans le fichier :", err)
+                } else {
+                    fmt.Println("Le texte a été ajouté avec succès !")
+                }
+                file.Close()
+            }
+        case 3:
+            err := ioutil.WriteFile("file.txt", []byte(""), 0644)
+            if err != nil {
+                fmt.Println("Erreur lors de la suppression du contenu du fichier :", err)
+            } else {
+                fmt.Println("Le contenu du fichier a été supprimé avec succès !")
+            }
+        case 4:
+            fmt.Println("Veuillez entrer le nouveau contenu du fichier :")
+            scanner := bufio.NewScanner(os.Stdin)
+            scanner.Scan()
+            text := scanner.Text()
+
+            err := ioutil.WriteFile("file.txt", []byte(text), 0644)
+            if err != nil {
+                fmt.Println("Erreur lors du remplacement du contenu du fichier :", err)
+            } else {
+                fmt.Println("Le contenu du fichier a été remplacé avec succès !")
+            }
+        case 5:
+            fmt.Println("Au revoir !")
+            return
+        default:
+            fmt.Println("Choix invalide")
+        }
+    }
+}
